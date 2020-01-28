11d079f1bf4a6bc0329b0d55a1e4b289e92cbe88
Code-JD <jonathan.d.herring@gmail.com>
2020-01-28 08:59:38 -0800

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,100 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: "purchase 1",
-                amount: 8.02
+                total: 19.40,
+                orderNumber: "A0048248343",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Jordan Hudgens",
+                    shippingAddress: "1234 West State Street"
+                }
+
             },
             {
                 _id: 1,
-                title: "purchase 2",
-                amount: 19.40
-            }
+                total: 5.20,
+                orderNumber: "B86kdi48343",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Maxwell Nelson",
+                    shippingAddress: "1337 Broad Way"
+                }
+
+            },
+            {
+                _id: 2,
+                total: 22.90,
+                orderNumber: "654sadf8e2",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Chanse Westby",
+                    shippingAddress: "396 Bellflower"
+                }
+
+            },
+            {
+                _id: 3,
+                total: 21.25,
+                orderNumber: "4s8fe2sf8e4",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Jim Butcher",
+                    shippingAddress: "13666 Bad Luck Street"
+                }
+
+            },
+            {
+                _id: 4,
+                total: 10.10,
+                orderNumber: "14f5v32d9t5",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Ben Teuan",
+                    shippingAddress: "8214 Klobbering Street"
+                }
+
+            },
+            {
+                _id: 5,
+                total: 3.70,
+                orderNumber: "j14df8h23j6",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "James Holden",
+                    shippingAddress: "1020 Rocinante Way"
+                }
+
+            },
+            {
+                _id: 6,
+                total: 2.70,
+                orderNumber: "5h4d19t3eg2",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Amos Burton",
+                    shippingAddress: "629 Mean Streets"
+                }
+
+            },
+            {
+                _id: 7,
+                total: 11.10,
+                orderNumber: "Bf4n2f5d4e",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Thomas Raith",
+                    shippingAddress: "50 Silver Lake Drive"
+                }
+
+            },
         ]
     })
 }
